b37b49605c4915a832406501d445fc4bc59823f4
MalagodiDaniel <daniel.malagodi1905@gmail.com>
2024-11-25 16:27:23 -0300

    first commit

---
+++ VariaveisPython.py
@@ -0,0 +1,18 @@
+
+#print("Digite o nome do seu jogador")
+
+#nickname = "Bundinha peluda"
+
+#print("Bem vindo " + nickname)
+#print(nickname + " entrou no servidor")
+
+############ ABA 2 ##################
+
+poteCafe = "Cafe Pelé"
+poteBiscoito = "Biscoito maizena"
+poteAcucar = "Açucar cristal"
+
+MENSAGEM_DA_VOVO = " Na cozinha da vovó tem:"
+
+print(MENSAGEM_DA_VOVO + poteAcucar + "," + poteBiscoito + "," + poteCafe)
+
