bcfd9347fee76a6033f5c218c5405168f5d7b8ae
MalagodiDaniel <daniel.malagodi1905@gmail.com>
2024-11-28 15:24:18 -0300

    Vetores em Python e JS

---
+++ vetores.py
@@ -0,0 +1,24 @@
+#times = ["Corimbas" , "Torneiras", "Trikas" , "Framengo"]
+
+#print(times[0])
+#print(times[3])
+#print("É o " + times[3])
+
+################# CÓD EM JS ###################
+#let times = ["Corimbas" , "Torneiras", "Trikas" , "Framengo"]
+
+#console.log(times[0])
+#console.log(times)
+
+################# MATRIZES ##################
+
+timesGrande = [
+    ["Corimbas" , "1910" , "Filé toshiba"],
+    ["Framengo" , "1895" , "Nassaum framenguista"]
+]
+
+#print(timesGrande[1])
+print(" Eu sou torcedor do " + timesGrande[0][0] +  ", que nasceu em " + timesGrande[0][1] + " e detem a maior torcida do Brasil, chamada de " + timesGrande[0][2])
+print(" Moro no Rio de Janeiro e sou torcedor do " + timesGrande[1][0] + " que foi criado em " + timesGrande[1][1] + " e eu faço parte da " + timesGrande[1][2])
+
+
