9c6fadba9dc80d3fcf895807a7bacefb30c636b4
MalagodiDaniel <daniel.malagodi1905@gmail.com>
2025-01-06 21:23:30 -0300

    Laços  de repetição FOR e WHILE.

--- IfElse.py
+++ IfElse.py
@@ -1,24 +1,48 @@
-ligou = False
+#ligou = False
 
-Robo = ""
+#Robo = ""
 
-if ligou:
-    print("Robo ativado")
-    Robo = "Robo funcionando normalmente"
-else:
-    print("Robo desligado")
-    Robo = "Falha no sistema"
+#if ligou:
+   #print("Robo ativado")
+   #Robo = "Robo funcionando normalmente"
+#else:
+    #print("Robo desligado")
+    #Robo = "Falha no sistema"
 
-print("Status do Robo: " + Robo)
+#print("Status do Robo: " + Robo)
 
 
-NiveisDeRotacaoMotor = 2000
+#NiveisDeRotacaoMotor = 2000
 
-if NiveisDeRotacaoMotor <= 10 : 
-        print("Motor com rotação baixa")
-elif NiveisDeRotacaoMotor >= 10 and NiveisDeRotacaoMotor <= 50:
-        print("Motor com rotação muito alta")
-else:
-        print("Motor do robo explodiu")
-    
+#if NiveisDeRotacaoMotor <= 10 : 
+        #print("Motor com rotação baixa")
+#elif NiveisDeRotacaoMotor >= 10 and NiveisDeRotacaoMotor <= 50:
+        #print("Motor com rotação muito alta")
+
+#else:
+        #print("Motor do robo explodiu")
+        
+        
+        
+        
+        
+####LAÇOS DE REPETIÇÃO### 
+
+##--FOR & WHILE--##
+
+#for contador in range(5):
+    #print(contador)
+
+## A função `range()` em Python é usada para gerar uma sequência de números.
+## É comumente utilizado em loops para iterar sobre uma série de valores 
+
+
+pontos_de_vida = 0
+
+for i in range(1, 11):
+    pontos_de_vida += 1
+    print("Tomou poção mágica" + str(i))
+
+print(str(pontos_de_vida) + " Totais ")
+
 
---
+++ for - while.py
@@ -0,0 +1,37 @@
+
+
+
+####LAÇOS DE REPETIÇÃO### 
+
+##--FOR & WHILE--##
+
+for contador in range(5):
+    print(contador)
+
+## A função `range()` em Python é usada para gerar uma sequência de números.
+## É comumente utilizado em loops para iterar sobre uma série de valores 
+
+
+pontos_de_vida = 0
+
+for i in range(1, 11):
+    pontos_de_vida += 1
+    print("Tomou poção mágica" + str(i))
+
+print(str(pontos_de_vida) + " Totais ")
+
+
+
+
+
+##while##
+i = 3
+
+while i <= 3:
+    print("olá")
+    i += 1
+    
+## O while (i <= 3)em JavaScript é traduzido diretamente para while i <= 3Python. A lógica é idêntica.## 
+
+
+
