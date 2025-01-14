a4f2bfb41aee609fd6d4b0021fefea68768919f6
MalagodiDaniel <daniel.malagodi1905@gmail.com>
2025-01-13 21:12:28 -0300

    DECLARAÇÃO E CHAMADA DE FUNÇÃO EM PYTHON

---
+++ Funções.py
@@ -0,0 +1,16 @@
+
+
+def torrar():
+    print("torrando pão")
+
+# Chamando a função
+torrar()
+
+
+def injetarPao():
+    print("inserindo pão na torradeira")
+    
+injetarPao()
+
+
+## NÃO COLOCAR NÚMEROS NO NOME DAS FUNÇÕES ##
