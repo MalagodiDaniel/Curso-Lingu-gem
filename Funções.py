571855b6f444653052d17d4acf84660a4a113203
MalagodiDaniel <daniel.malagodi1905@gmail.com>
2025-01-14 21:18:13 -0300

    DECLARANDO E CHAMANDO FUNÇÕES EM PYTHON

--- Funções.py
+++ Funções.py
@@ -14,3 +14,22 @@ injetarPao()
 
 
 ## NÃO COLOCAR NÚMEROS NO NOME DAS FUNÇÕES ##
+
+
+## FUNÇÕES COM PARAMETROS ##
+
+
+
+def torrar1(pao):
+    print("Torrada feita com " + pao)
+
+torrar1("Pão frances")
+
+
+
+def torrar1(pao, nome = "cliente"):
+    print("Torrada feita com " + pao)
+    print("ELe é um pedido do cliente " + nome)
+
+
+torrar1("Pão frances " , "Malagodi")
